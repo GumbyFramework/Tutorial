c3e94a57f0bae75f3748d5df22c6a841c3de6af7
Adam Chambers <adammchambers@gmail.com>
2013-10-02 23:39:29 -0400

    Quote form validation

--- js/main.js
+++ js/main.js
@@ -5,4 +5,39 @@ Gumby.ready(function() {
 	$('#highlights').on('gumby.onShuffle', function() {
 		Gumby.initialize('skiplink');
 	});
+
+	// quote form validation
+	$('.get-quote form').validation({
+		required: [
+			{
+				name: 'name'
+			},
+			{
+				name: 'phone'
+			},
+			{
+				name: 'email',
+				// email must contain @
+				validate: function($el) {
+					return $el.val().indexOf('@') > -1;
+				}
+			},
+			{
+				name: 'version',
+				// must pick version with valid value
+				validate: function($el) {
+					return $el.val() !== '#';
+				}
+			}
+		],
+		// validation passed
+		submit: function(formData) {
+			Gumby.debug(formData);
+			alert("Succes! Posting form...");
+		},
+		// validation failed
+		fail: function() {
+			alert("Form validation failed...");
+		}
+	});
 });
